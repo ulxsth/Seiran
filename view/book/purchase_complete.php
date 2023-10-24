34765757ba99b796be5174ce82221843b920580c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-24 11:38:35 +0900

    update: 内容をデザインカンプに同期

---
+++ view/book/purchase_complete.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>小説購入完了 | Seiran</title>
+
+  <!-- reset.css destyle -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
+
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ view/user/edit.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- TODO: ここに小説名を挿入 -->
+  <title>プロフィール編集 | Seiran</title>
+
+  <!-- reset.css destyle -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
+
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
