d49339c20cb4e70d626a57767db51781b3c1244c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-24 11:57:24 +0900

    fix: cssの読み込みパスを相対パスに変更

--- view/book/show.php
+++ view/book/show.php
@@ -10,7 +10,7 @@
   <!-- reset.css destyle -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css" />
 
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="../../css/app.css">
 </head>
 
 <body>
