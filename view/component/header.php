1cb99a0bb4bdbe22b74bb3b3822bf553ac9772ed
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-24 13:58:52 +0900

    refactor: rename

--- view/component/header.php
+++ view/component/header.php
@@ -1,3 +1,17 @@
 <header>
-  <h1>test</h1>
+  <a href="">
+    <img src="/seiran/assets/img/logo.svg" alt="Service logo">
+  </a>
+
+  <!-- TODO: ユーザのログイン状態によって切り替え -->
+  <!-- ログイン時 -->
+  <?php if (true) : ?>
+    
+
+  <!-- 未ログイン時 -->
+  <?php else : ?>
+    <button type="button" onClick="location.href='/seiran/view/auth/login_id.php'">ログイン</button>
+    <button type="button" onClick="location.href='/seiran/view/auth/signin.php'">新規登録</button>
+    <p>test2</p>
+  <?php endif; ?>
 </header>
