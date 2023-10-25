71bc6dc0e0d13a28766cf3a8ba27e0d9c0c21b9c
sota022 <2201028@s.asojuku.ac.jp>
2023-10-25 12:50:38 +0900

    小説

--- view/book/show.php
+++ view/book/show.php
@@ -5,10 +5,19 @@
   <?php require_once '../component/head.php'; ?>
   <!-- TODO: ここに小説名を挿入 -->
   <title>小説詳細 | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/book/show.css">
 </head>
 
 <body>
   <?php require_once '../component/header.php'; ?>
 </body>
-
+  <main>
+    <img src="/seiran/assets/img/">
+    <h1>test_1</h1>
+    <p>初版投稿日:20XX-XX-XX</p>
+    <p>更新日:20XX-XX-XX</p>
+    <p><img src="/seiran/assets/img/">テストユーザー（test）</p>
+    <p>これは概要です</p>
+    <button type="button">読む</button>
+  </main>
 </html>
