4eb06187ce1933f6e84de9ab8124c928e07d9a02
yotu <114195789+ulxsth@users.noreply.github.com>
2023-10-25 14:48:04 +0900

    refactor: DTOへの詰め替え処理を関数として隔離

--- src/repository/BookRepository.php
+++ src/repository/BookRepository.php
@@ -41,7 +41,47 @@ class BookRepository {
 
     // 結果の取得,DTOに詰め替え
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $book = $this->toDto($result);
+
+    return $book;
+  }
+
+  /**
+   * IDが一致する本の情報を更新する
+   * @param BookDTO $book
+   * @return void
+   */
+  public function update($book) {
+    // SQLの準備
+    $sql = 'UPDATE book SET thumbnail_path = :thumbnail_path, name = :name, registered_at = :registered_at, description = :description, user_id = :user_id, price = :price, is_public = :is_public WHERE id = :id';
+
+    // SQLの実行
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindValue(':thumbnail_path', $book->getThumbnailPath(), PDO::PARAM_STR);
+    $stmt->bindValue(':name', $book->getName(), PDO::PARAM_STR);
+    $stmt->bindValue(':registered_at', $book->getRegisteredAt(), PDO::PARAM_STR);
+    $stmt->bindValue(':description', $book->getDescription(), PDO::PARAM_STR);
+    $stmt->bindValue(':user_id', $book->getUserId(), PDO::PARAM_INT);
+    $stmt->bindValue(':price', $book->getPrice(), PDO::PARAM_INT);
+    $stmt->bindValue(':is_public', $book->getIsPublic(), PDO::PARAM_INT);
+    $stmt->bindValue(':id', $book->getId(), PDO::PARAM_INT);
+    $stmt->execute();
+  }
+
+  public function deleteBook($id) {
+    // TODO: Implement deleteBook method
+  }
+
+  /**
+   * PDOStatementの処理結果のうち一行を引数にとり、
+   * BookDTOに詰め替えて返す。
+   * @param mixed $stmt
+   * @return BookDTO
+   */
+  private function toDto($result)
+  {
     $book = new BookDTO($result['id']);
+
     $book->setThumbnailPath($result['thumbnail_path']);
     $book->setName($result['name']);
     $book->setRegisteredAt($result['registered_at']);
@@ -51,14 +91,6 @@ class BookRepository {
     $book->setIsPublic($result['is_public']);
 
     return $book;
-  }
-
-  public function updateBook($book) {
-
-  }
-
-  public function deleteBook($id) {
-    // TODO: Implement deleteBook method
   }
 }
 
