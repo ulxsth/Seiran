388af654c5267e8f09792f5c15a388681b5461a2
massan02 <tankful-secure0c@icloud.com>
2023-10-25 14:08:15 +0900

    タスク分

---
+++ src/repository/UserDTO.php
@@ -0,0 +1,64 @@
+<?php
+
+class UserDTO {
+    private $userId;
+    private $email;
+    private $passwordHash;
+    private $name;
+    private $registeredAt;
+    private $isPublic;
+    private $description;
+
+    public function __construct($userId) {
+        $this->userId = $userId;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getPasswordHash() {
+        return $this->passwordHash;
+    }
+
+    public function setPasswordHash($passwordHash) {
+        $this->passwordHash = $passwordHash;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getRegisteredAt() {
+        return $this->registeredAt;
+    }
+
+    public function setRegisteredAt($registeredAt) {
+        $this->registeredAt = $registeredAt;
+    }
+
+    public function getIsPublic() {
+        return $this->isPublic;
+    }
+
+    public function setIsPublic($isPublic) {
+        $this->isPublic = $isPublic;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+}
+?>
---
+++ src/repository/UserRepository.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once(__DIR__ . '/../config/Database.php');
+require_once(__DIR__ . '/../models/User.php');
+
+class UserRepository {
+    private $conn;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function getAllUsers() {
+        $query = 'SELECT * FROM users';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $users = array();
+        while ($row = $result->fetch_assoc()) {
+            $user = new User($row['id'], $row['name'], $row['email']);
+            array_push($users, $user);
+        }
+
+        return $users;
+    }
+
+    public function getUserById($id) {
+        $query = 'SELECT * FROM users WHERE id = ?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 0) {
+            return null;
+        }
+
+        $row = $result->fetch_assoc();
+        $user = new User($row['id'], $row['name'], $row['email']);
+
+        return $user;
+    }
+
+    public function createUser($name, $email) {
+        $query = 'INSERT INTO users (name, email) VALUES (?, ?)';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('ss', $name, $email);
+        $stmt->execute();
+
+        return $stmt->insert_id;
+    }
+
+    public function updateUser($id, $name, $email) {
+        $query = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('ssi', $name, $email, $id);
+        $stmt->execute();
+
+        return $stmt->affected_rows > 0;
+    }
+
+    public function deleteUser($id) {
+        $query = 'DELETE FROM users WHERE id = ?';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        return $stmt->affected_rows > 0;
+    }
+}
