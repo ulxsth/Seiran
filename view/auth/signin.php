f0fc1f0a37762afd01824682180de8686b8ef2e2
satoukazunobu <144978943+satoukazunobu@users.noreply.github.com>
2023-10-25 14:13:55 +0900

    signin

--- view/auth/signin.php
+++ view/auth/signin.php
@@ -3,14 +3,35 @@
 
 <head>
   <?php require_once '../component/head.php'; ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>新規登録 | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/auth/signin.css">
 </head>
 
 <body>
-  <main>
-    <!-- ここに内容 -->
-
-  </main>
+  <div class="gray-box">
+    <div class="blue-box">
+      <!-- ここにコンテンツを追加 -->
+      <main>
+        <!-- ここに内容 -->
+        <h1>新規登録</h1>
+        <p>ユーザーID</p>
+        <input type="text" name="userID" id="">
+        <p>ユーザーネーム</p>
+        <input type="text" name="username" id="">
+        <p>メールアドレス</p>
+        <input type="text" name="mailaddress" id="">
+        <p>パスワード</p>
+        <input type="text" name="password" id="">
+        <p>パスワード(確認)</p>
+        <input type="text" name="password" id="">
+        <button type="submit">新規登録</button>
+        <button type="login">もしくは<br>ログイン</button>
+      </main>
+    </div>
+  </div>
+  
 </body>
 
 </html>
