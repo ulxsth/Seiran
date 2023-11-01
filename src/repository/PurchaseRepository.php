2234fcec9d9cfc90199d2f0a8515fad127efc4f9
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-01 16:10:47 +0900

    refactor: findByBookIdを修正

--- src/repository/PurchaseRepository.php
+++ src/repository/PurchaseRepository.php
@@ -49,24 +49,27 @@ class PurchaseRepository {
         return $purchases;
     }
 
+    /**
+     * 指定した本のIDに一致する購入情報を取得する
+     * @param int $bookId
+     * @return array[PurchaseDto] purchases
+     */
     public function findByBookId($bookId) {
-        $query = 'SELECT * FROM purchases WHERE bookId = ?';
-        $stmt = $this->pdo->prepare($query);
-        $stmt->bind_param('s', $bookId);
+        // SQLの準備
+        $sql = 'SELECT * FROM purchases WHERE bookId = :book_id';
+        $stmt = $this->pdo->prepare($sql);
+
+        // SQLの実行
+        $stmt->bindParam(':book_id', $bookId);
         $stmt->execute();
-        $result = $stmt->get_result();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        if ($result->num_rows == 0) {
-            return null;
-        }
-
-        // 購入情報が複数存在する可能性があるため、配列に格納します。
+        // 購入情報が複数存在する場合があるため、配列に格納する
         $purchases = [];
-        while ($row = $result->fetch_assoc()) {
-            $purchase = new PurchaseDTO($row['userId'], $row['bookId'], $row['current_price'], $row['purchase_at']);
+        foreach ($result as $row) {
+            $purchase = $this->toDto($row);
             array_push($purchases, $purchase);
         }
-
         return $purchases;
     }
 
