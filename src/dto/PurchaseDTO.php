03d7ceb86be552d9cfaf84522c1beaf2f2fbd63c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-01 14:52:23 +0900

    refactor

--- src/dto/PurchaseDTO.php
+++ src/dto/PurchaseDTO.php
@@ -1,33 +1,30 @@
 <?php
 
-class PurchaseDTO{
+class PurchaseDTO
+{
     private $userId;
     private $bookId;
     private $current_price;
     private $purchase_at;
 
-    public function __construct($userId,$bookId,$current_price,$purchase_at){
+    public function __construct($userId, $bookId, $current_price, $purchase_at) {
         $this->userId = $userId;
         $this->bookId = $bookId;
         $this->current_price = $current_price;
         $this->purchase_at = $purchase_at;
     }
 
-    public function getUserId(){
+    // getter
+    public function getUserId() {
         return $this->userId;
     }
-
-    public function getBookId(){
+    public function getBookId() {
         return $this->bookId;
     }
-
-    public function getCurrentPrice(){
+    public function getCurrentPrice() {
         return $this->current_price;
     }
-
-    public function getPurchaseAt(){
+    public function getPurchaseAt() {
         return $this->purchase_at;
     }
 }
-
-?>
