a80b321ce18c430700f1626d08aeda9d0b271596
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-01 13:58:44 +0900

    update: UserDTOの整形

--- src/dto/UserDTO.php
+++ src/dto/UserDTO.php
@@ -1,5 +1,4 @@
 <?php
-
 class UserDTO {
     private $id;
     private $email;
@@ -13,56 +12,53 @@ class UserDTO {
         $this->id = $id;
     }
 
-    public function getId() {
+    // getter
+    public function getId()
+    {
         return $this->id;
     }
-
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
-
-    public function setEmail($email) {
-        $this->email = $email;
-    }
-
-    public function getPasswordHash() {
+    public function getPasswordHash()
+    {
         return $this->passwordHash;
     }
-
-    public function setPasswordHash($passwordHash) {
-        $this->passwordHash = $passwordHash;
-    }
-
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getRegisteredAt() {
+    public function getRegisteredAt()
+    {
         return $this->registeredAt;
     }
-
-    public function setRegisteredAt($registeredAt) {
-        $this->registeredAt = $registeredAt;
-    }
-
-    public function getIsPublic() {
+    public function getIsPublic()
+    {
         return $this->isPublic;
     }
-
-    public function setIsPublic($isPublic) {
-        $this->isPublic = $isPublic;
-    }
-
-    public function getDescription() {
+    public function getDescription()
+    {
         return $this->description;
     }
 
+    // setter
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    public function setPasswordHash($passwordHash) {
+        $this->passwordHash = $passwordHash;
+    }
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function setRegisteredAt($registeredAt) {
+        $this->registeredAt = $registeredAt;
+    }
+    public function setIsPublic($isPublic) {
+        $this->isPublic = $isPublic;
+    }
     public function setDescription($description) {
         $this->description = $description;
     }
 }
-?>
