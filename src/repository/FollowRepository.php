ad2e976b31662c64852a85606c20eb76b9bd95d1
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-10 11:32:52 +0900

    Create FollowRepository.php

---
+++ src/repository/FollowRepository.php
@@ -0,0 +1,60 @@
+<?php
+require_once( __DIR__ .'./../util/PdoManager.php');
+require( __DIR__ . '/../dto/FollowDTO.php');
+
+class FollowRepository {
+  private static $pdo = PdoManager::getPdo();
+
+  /**
+   * フォロー情報を取得する
+   * @param FollowDTO $dto
+   * @return void
+   */
+  public static function insert($dto) {
+    // SQLの準備
+    $sql = 'INSERT INTO follow (user_id, follow_user_id) VALUES (:user_id, :follow_user_id)';
+
+    // SQLの実行
+    $stmt = self::$pdo->prepare($sql);
+    $stmt->bindValue(':followee_id', $dto->getFolloweeId(), PDO::PARAM_INT);
+    $stmt->bindValue(':follower_id', $dto->getFollowerId(), PDO::PARAM_INT);
+    $stmt->execute();
+  }
+
+  /**
+   * フォローしているかどうか取得する
+   * @param FollowDTO $dto
+   * @return boolean
+   */
+  public static function isExist($dto) {
+    // SQLの準備
+    $sql = 'SELECT * FROM follow WHERE user_id = :user_id AND follow_user_id = :follow_user_id';
+
+    // SQLの実行
+    $stmt = self::$pdo->prepare($sql);
+    $stmt->bindValue(':followee_id', $dto->getFolloweeId(), PDO::PARAM_INT);
+    $stmt->bindValue(':follower_id', $dto->getFollowerId(), PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $result !== false;
+  }
+
+  /**
+   * フォロー情報を削除する
+   * @param FollowDTO $dto
+   * @return void
+   */
+  public static function delete($dto)
+  {
+    // SQLの準備
+    $sql = 'DELETE FROM follow WHERE user_id = :user_id AND follow_user_id = :follow_user_id';
+
+    // SQLの実行
+    $stmt = self::$pdo->prepare($sql);
+    $stmt->bindValue(':followee_id', $dto->getFolloweeId(), PDO::PARAM_INT);
+    $stmt->bindValue(':follower_id', $dto->getFollowerId(), PDO::PARAM_INT);
+    $stmt->execute();
+  }
+}
+?>
