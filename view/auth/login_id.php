0372e72b02a0efda82e347450239cef69891b6a5
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-10 13:40:10 +0900

    refactor: 不要なdivを削除

--- view/auth/login_id.php
+++ view/auth/login_id.php
@@ -5,31 +5,28 @@
   <?php require_once '../component/head.php'; ?>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>検索 | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/app.css">
   <link rel="stylesheet" href="/seiran/css/auth/login_id.css">
 </head>
 
 <body>
-  <div class="gray-box">
-    <div class="blue-box">
-    <!-- ここにコンテンツを追加 -->
-      <main>
-        <!-- ここに内容 -->
-        <h1>ログイン</h1>
-        <form action="post">
-          <p>ユーザーID</p>
-          <input type="text" name="userID" id="">
-          <div class="right-align">
-            <a href="login_email.php">メールアドレスでログインする</a>
-          </div>
-          <p>パスワード</p>
-          <input type="text" name="password" id="">
-        </form>
-        <button type="submit">ログイン</button>
-        <button type="login">もしくは<br>新規登録</button>
-        
-      </main>
-    </div>
-  </div>
+  <main>
+    <h1>ログイン</h1>
+
+    <form method="#" action="post">
+      <label for="user_id">ユーザーID</label>
+      <input type="text" name="user_id">
+
+      <div class="right-align">
+        <a href="login_email.php">メールアドレスでログインする</a>
+      </div>
+
+      <label for="password">パスワード</label>
+      <input type="password" name="password">
+    </form>
+    <button type="submit">ログイン</button>
+    <button type="login">もしくは<br>新規登録</button>
+  </main>
 </body>
 
 </html>
