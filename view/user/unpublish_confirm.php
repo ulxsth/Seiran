359a46a69c267e1671c8bfabb6a4cbf12662a8d9
satoukazunobu <144978943+satoukazunobu@users.noreply.github.com>
2023-11-14 11:46:36 +0900

    a

---
+++ view/user/unpublish_confirm.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php require_once '../component/head.php'; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>非公開確認 | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/app.css">
+  <link rel="stylesheet" href="/seiran/css/user/unpublish_confirm.css">
+</head>
+
+<body>
+  <main>
+    <h1>非公開前確認</h1>
+    <h2>小説を非公開にします。よろしいですか？</h2>
+  </main>
+</body>
+
+</html>
