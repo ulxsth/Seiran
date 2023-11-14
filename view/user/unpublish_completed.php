265024581c77d81b8a8f86b25d5768a8d930a59a
satoukazunobu <144978943+satoukazunobu@users.noreply.github.com>
2023-11-14 13:58:43 +0900

    a

---
+++ view/user/unpublish_completed.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php require_once '../component/head.php'; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログイン | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/app.css">
+  <link rel="stylesheet" href="/seiran/css/user/unpublish_completed.css">
+</head>
+
+<body>
+  <main>
+    <p>非公開にしました。</p>
+    <p>非公開になった小説はいつでも「下書き一覧」から再投稿できます。</p>
+    <div class="control">
+      <button type="submit" class="btn button-submit">ホームへ</button>
+    </div>
+</main>
+</body>
+
+</html>
