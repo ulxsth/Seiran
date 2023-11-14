887cb4480e36aedee302974674a58172d3fec938
satoukazunobu <144978943+satoukazunobu@users.noreply.github.com>
2023-11-14 11:53:19 +0900

    a

--- view/user/unpublish_confirm.php
+++ view/user/unpublish_confirm.php
@@ -13,6 +13,13 @@
   <main>
     <h1>非公開前確認</h1>
     <h2>小説を非公開にします。よろしいですか？</h2>
+    <p>TIPS:非公開にした小説は、エディターの「下書き一覧」から再度投稿できます。</p>
+    <h1>タイトル</h1>
+    <h1>ここにタイトル</h1>
+    <div class="control">
+      <button type="submit" class="btn button-submit">非公開にする</button>
+      
+    </div>
   </main>
 </body>
 
