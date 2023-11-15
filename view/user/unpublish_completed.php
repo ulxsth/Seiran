3391b4ab5226e1c93913f73abea16c060f1a53f3
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-15 16:28:48 +0900

    refactor: 小説非公開完了画面のCSSをBulmaで整形

--- view/user/unpublish_completed.php
+++ view/user/unpublish_completed.php
@@ -11,17 +11,19 @@
 
 <body>
   <?php require_once '../component/header.php'; ?>
-  <main>
-  <img src="/seiran/assets/img/sendback.png" alt="sendback_icon" class="img">
+  <main class="has-text-centered">
+    <figure class="image is-256x256 mx-auto mb-6">
+      <img src="/seiran/assets/img/sendback.png" alt="sendback_icon">
+    </figure>
 
-    <div class="control">
+    <div class="control mb-6">
       <p>非公開にしました。</p>
       <p>非公開になった小説はいつでも「下書き一覧」から再投稿できます。</p>
     </div>
 
-    <div class="control">
+    <div class="control ">
       <button type="submit" class="button is-link is-outlined">ホームへ</button>
     </div>
   </main>
-</body>
+
 </html>
