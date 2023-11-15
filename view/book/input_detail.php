e8d54868fa92d656e74c848337c66e71e6d6e8a7
Meiki <mickey.0103.eiking@outlook.jp>
2023-11-15 14:20:50 +0900

    html終わりcssは調整中

--- view/book/input_detail.php
+++ view/book/input_detail.php
@@ -3,11 +3,38 @@
 
 <head>
   <?php require_once '../component/head.php'; ?>
+  <link rel="stylesheet" href="../../css/book/input_detail.css">
   <title>小説概要入力 | Seiran</title>
 </head>
 
 <body>
   <?php require_once '../component/header.php'; ?>
+  <div class="back">
+        <h2>小説を作成する</h2>
+        <p>タイトル</p>
+        <input type="text" name="name"><br>
+        <p>概要</p>
+        <textarea rows="5" cols="100">
+        </textarea><br>
+        <div class="money">
+            <p>価格</p>
+            <input type="number" name="price">
+        </div>
+        <div class="category">
+            <p>カテゴリー</p>
+            <select name="category">
+                <option value="1">マッサン</option>
+                <option value="2">まっさん</option>
+                <option value="3">massan</option>
+            </select>
+        </div>
+        <div class="button">
+            <input type="button" value="作成">
+        </div>
+        <div class="text">
+        *作成後にエディタ画面に遷移します
+        </div>
+    </div>
 </body>
 
 </html>
