d064ecd0f55310797da76e3c02ae0ca94d48c276
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-16 10:21:44 +0900

    refactor: Bulmaの反映、余白の調整

--- view/book/input_detail.php
+++ view/book/input_detail.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php require_once '../component/head.php'; ?>
-    <link rel="stylesheet" href="../../css/book/input_detail.css">
+    <!-- <link rel="stylesheet" href="../../css/book/input_detail.css"> -->
     <title>小説概要入力 | Seiran</title>
     <?php require_once '../component/header.php'; ?>
     <link rel="stylesheet" href="/seiran/css/app.css">
@@ -11,32 +11,49 @@
 
 <body>
     <?php require_once '../component/header.php'; ?>
-    <div class="back">
-        <h2>小説を作成する</h2>
-        <p>タイトル</p>
-        <input type="text" name="name"><br>
-        <p>概要</p>
-        <textarea rows="5" cols="100">
-        </textarea><br>
-        <div class="money">
-            <p>価格</p>
-            <input type="number" name="price">
-        </div>
-        <div class="category">
-            <p>カテゴリー</p>
-            <select name="category">
-                <option value="1">マッサン</option>
-                <option value="2">まっさん</option>
-                <option value="3">massan</option>
-            </select>
-        </div>
-        <div class="button">
-            <input type="button" value="作成">
-        </div>
-        <div>
-            ※作成後にエディタ画面に遷移します
-        </div>
-    </div>
+    <main>
+        <section class="has-text-centered">
+            <h1>小説を作成する</h1>
+        </section>
+
+        <form action="#" method="POST">
+            <div class="container mb-6">
+                <div class="field">
+                    <label for="title">タイトル</label>
+                    <input class="input" type="text" name="title"><br>
+                </div>
+                <div class="field">
+                    <label for="description">概要</label>
+                    <textarea name="description" class="textarea"></textarea>
+                </div>
+                <div class="field is-flex">
+                    <section class="mr-6">
+                        <label for="price">価格</label>
+                        <input class="input" type="number" name="price">
+                    </section>
+                    <section>
+                        <label for="category">カテゴリー</label>
+                        <div class="select">
+                            <select name="category">
+                                <option value="1">マッサン</option>
+                                <option value="2">まっさん</option>
+                                <option value="3">massan</option>
+                            </select>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+            <div class=" field flex-center">
+                <button type="submit" class="button is-primary px-6">作成</button>
+            </div>
+
+            <div>
+                <p class="has-text-grey">※作成後にエディタ画面に遷移します</p>
+            </div>
+        </form>
+    </main>
+
 </body>
 
 </html>
