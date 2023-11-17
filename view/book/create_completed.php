662ab5e91fff62e1d6c96aae219de7e5824c0690
Meiki <mickey.0103.eiking@outlook.jp>
2023-11-17 14:42:34 +0900

    完成

--- view/book/create_completed.php
+++ view/book/create_completed.php
@@ -8,6 +8,23 @@
 
 <body>
   <?php require_once '../component/header.php'; ?>
+  <main>
+  <div class="back">
+    <section class="container">
+    <img src="../../assets/img/done.svg"  alt="done logo">
+    </section>
+    <div class="is-size-2 has-text-link-dark mb-4">
+      DONE!
+    </div>
+    <div class="is-size-2 mb-4">
+    小説が投稿されました。
+    </div>
+    <div class="is-justify-content-space-evenly mb-4" style="display: flex; justify-content: space-between;">
+  <button class="button is-info is-outlined is-medium" type="submit">詳細ページへ</button>
+  <button class="button is-info is-outlined is-medium" type="submit">ホームへ</button>
+</div>
+
+  </main>
 </body>
 
 </html>
