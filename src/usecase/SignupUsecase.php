643b3df3efe607f5a5f37a70f9a532fa870213f1
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-17 14:08:04 +0900

    fix: POSTから値を渡すように修正

--- src/usecase/SignupUsecase.php
+++ src/usecase/SignupUsecase.php
@@ -4,21 +4,21 @@ $repository = new UserRepository();
 
 // バリデーション
 // ユーザーIDが既に存在するか
-$user = $repository->findById($id);
+$user = $repository->findById($_POST['id']);
 if($user != null) {
   // TODO: エラーメッセージを表示
   header("Location: /seiran/view/auth/signup.php");
 }
 
 // メールアドレスが既に存在するか
-$user = $repository->findByEmail($email);
+$user = $repository->findByEmail($_POST['email']);
 if ($user != null) {
   // TODO: エラーメッセージを表示
   header("Location: /seiran/view/auth/signup.php");
 }
 
 // 登録処理
-$repository->insert($id, $name, $email, $password);
+$repository->insert($_POST['id'], $_POST['name'], $_POST['email'], $_POST['password']);
 
 // ホーム画面に遷移
 header("Location: /seiran/view/book/info.php");
