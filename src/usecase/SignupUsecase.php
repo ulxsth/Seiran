8a8269497dc777e7632bd9ebfa6e6d3ed41187a6
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-17 12:00:19 +0900

    update: SignupUseCaseの主処理を実装

--- src/usecase/SignupUsecase.php
+++ src/usecase/SignupUsecase.php
@@ -1,11 +1,25 @@
 <?php
-require_once dirname(__FILE__, 2) . "/model/User.php";
+require_once dirname(__FILE__, 2) . "/repository/UserRepository.php";
+$repository = new UserRepository();
 
 // バリデーション
 // ユーザーIDが既に存在するか
+$user = $repository->findById($id);
+if($user != null) {
+  // TODO: エラーメッセージを表示
+  header("Location: /seiran/view/auth/signup.php");
+}
+
 // メールアドレスが既に存在するか
+$user = $repository->findByEmail($email);
+if ($user != null) {
+  // TODO: エラーメッセージを表示
+  header("Location: /seiran/view/auth/signup.php");
+}
 
 // 登録処理
+$repository->insert($id, $name, $email, $password);
 
 // ホーム画面に遷移
+header("Location: /seiran/view/book/info.php");
 ?>
