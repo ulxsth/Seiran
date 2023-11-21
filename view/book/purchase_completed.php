633602c13e3f3c331a0f3552b87e85db6d41d25a
satoukazunobu <144978943+satoukazunobu@users.noreply.github.com>
2023-11-21 14:02:22 +0900

    a

--- view/book/purchase_completed.php
+++ view/book/purchase_completed.php
@@ -23,7 +23,7 @@
     </div>
 
     <div class="control">
-        <button type="submit" class="button is-link is-outlined px-6">読む</button>
+        <button type="submit" class="button is-primary">読む</button>
         <button type="submit" class="button is-primary">ホームへ</button>
     </div>
 </main>
