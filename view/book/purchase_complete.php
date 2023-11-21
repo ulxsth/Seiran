dc4bd49970847417f1306acb243cf332a12b7a1d
satoukazunobu <144978943+satoukazunobu@users.noreply.github.com>
2023-11-21 12:28:41 +0900

    s

--- view/book/purchase_complete.php
+++ view/book/purchase_complete.php
@@ -15,18 +15,18 @@
   <?php require_once '../component/header.php'; ?>
   <main class="has-text-centered">
     <figure class="image is-256x256 mx-auto mb-6">
-      <img src="/seiran/assets/img/MicrosoftTeams-image (1).png" alt="sendback_icon">
+        <img src="/seiran/assets/img/MicrosoftTeams-image (1).png" alt="sendback_icon">
     </figure>
 
     <div class="control mb-6">
-      <p>購入が完了しました！</p>
+        <p>購入が完了しました！</p>
     </div>
 
     <div class="control">
-      <button type="submit" class="button is-link is-outlined px-6">読む</button>
-      <button type="submit" class="button is-link is-outlined px-6">ホームへ</button>
+        <button type="submit" class="button is-link is-outlined px-6">読む</button>
+        <button type="submit" class="button is-link is-outlined px-6">ホームへ</button>
     </div>
-  </main>
+</main>
 </body>
 
 </html>
