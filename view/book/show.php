a125bf0cf791fdc3f1a52d905ccdab58f5398f5e
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-21 12:21:23 +0900

    fix: なんかいろいろ

--- view/book/show.php
+++ view/book/show.php
@@ -14,19 +14,30 @@
 <body>
   <?php require_once '../component/header.php'; ?>
   <main>
-    <div class="left">
-      <div class="book_thumbnail mb-6">
-        <img src="https://via.placeholder.com/512x512" alt="book">
+    <div class="is-flex">
+      <div class="left mr-6">
+        <div class="book_thumbnail mb-6">
+          <img src="https://via.placeholder.com/512x512" alt="book">
+        </div>
+        <h2 class="has-text-right">100000 円</h2>
+        <a href="#" id="button_read" class="button is-primary">読む</a>
       </div>
-      <h2 class="has-text-right">100000 円</h2>
-      <a href="#" id="button_read" class="button is-primary">読む</a>
-    </div>
-    <div class="right">
-      <p class="toukou"><span class="has-text-weight-bold">初版投稿日</span>：20XX-XX-XX</p>
-      <p class="kousin"><span class="has-text-weight-bold">更新日</span>：20XX-XX-XX</p>
-      <p class="title">test_1</p>
-      <p><img src="/seiran/assets/img/">テストユーザー（test）</p>
-      <p class="text">これは概要です</p>
+      <div class="right">
+        <p class="has-text-right">
+          <span class="has-text-weight-bold">初版投稿日</span>：20XX-XX-XX
+        </p>
+        <p class="has-text-right">
+          <span class="has-text-weight-bold">更新日</span>：20XX-XX-XX
+        </p>
+        <p class="title">test_1</p>
+        <div class="user mb-3">
+          <figure class="image mr-3">
+            <img class="is-rounded" src="https://via.placeholder.com/32x32" alt="user icon">
+          </figure>
+          <span class="has-text-grey">テストユーザー（test）</span>
+        </div>
+        <p>これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。これは概要です。</p>
+      </div>
     </div>
   </main>
 </body>
