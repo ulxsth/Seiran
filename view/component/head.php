47709997c9d506577cc57eb4d63d30a2f214a06b
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-21 14:18:05 +0900

    fix: app.cssを最後に読み込むように

--- view/component/head.php
+++ view/component/head.php
@@ -1,10 +1,11 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" href="/seiran/css/app.css">
 
 <!-- load fontawesome -->
 <link href="https://use.fontawesome.com/releases/v6.4.2/css/all.css" rel="stylesheet">
 
 <!-- load bulma -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css">
+
+<link rel="stylesheet" href="/seiran/css/app.css">
