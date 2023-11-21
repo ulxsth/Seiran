c449ed0eb179338e6945ad75d2dba0681fa351db
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-21 11:57:58 +0900

    refactor: flex用のdivが冗長だったのでmainに肩代わりした

--- view/book/show.php
+++ view/book/show.php
@@ -14,21 +14,19 @@
 <body>
   <?php require_once '../component/header.php'; ?>
   <main>
-    <div class="flex">
-      <div class="left">
-        <div class="book_thumbnail">
-          <img src="https://via.placeholder.com/512x512" alt="book">
-        </div>
-        <p class="nedan">値段表示</p>
-        <a href="#" class="button">読む</a>
+    <div class="left">
+      <div class="book_thumbnail">
+        <img src="https://via.placeholder.com/512x512" alt="book">
       </div>
-      <div class="right">
-        <p class="toukou"><span class="has-text-weight-bold">初版投稿日</span>：20XX-XX-XX</p>
-        <p class="kousin"><span class="has-text-weight-bold">更新日</span>：20XX-XX-XX</p>
-        <p class="title">test_1</p>
-        <p><img src="/seiran/assets/img/">テストユーザー（test）</p>
-        <p class="text">これは概要です</p>
-      </div>
+      <p class="nedan">値段表示</p>
+      <a href="#" class="button">読む</a>
+    </div>
+    <div class="right">
+      <p class="toukou"><span class="has-text-weight-bold">初版投稿日</span>：20XX-XX-XX</p>
+      <p class="kousin"><span class="has-text-weight-bold">更新日</span>：20XX-XX-XX</p>
+      <p class="title">test_1</p>
+      <p><img src="/seiran/assets/img/">テストユーザー（test）</p>
+      <p class="text">これは概要です</p>
     </div>
   </main>
 </body>
