b6bd5228cf853215ba17f732d1d8297112637ef6
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-22 11:41:05 +0900

    update: content内のCSSを整えた

--- view/book/editor.php
+++ view/book/editor.php
@@ -6,19 +6,22 @@
 <head>
   <?php require_once '../component/head.php'; ?>
   <title>編集中 | Seiran</title>
-  <link rel="stylesheet" href="/seiran/css/editor.css">
+  <link rel="stylesheet" href="/seiran/css/book/editor.css">
 </head>
 
 <body>
   <div class="columns">
-    <div class="column is-one-fifth">
+    <div class="sidebar column is-one-fifth">
       <aside class="menu">
       </aside>
     </div>
     <div class="column">
       <div class="content">
-        <input class="input" type="text" placeholder="タイトルを入力">
-        <textarea class="textarea" placeholder="本文を入力..."></textarea>
+        <input class="input-title input" type="text" placeholder="タイトルを入力">
+        <textarea class="input-content textarea" placeholder="本文を入力..."></textarea>
+        <div class="toolbar">
+
+        </div>
       </div>
     </div>
   </div>
