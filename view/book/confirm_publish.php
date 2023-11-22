ce7c87fe2ca74b0eb27170ff91d7fdce08039155
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-22 09:46:59 +0900

    refactor

--- view/book/confirm_publish.php
+++ view/book/confirm_publish.php
@@ -8,24 +8,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>公開前確認 | Seiran</title>
   <?php require_once '../component/head.php'; ?>
-  <link rel="stylesheet" href="/seiran/css/app.css">
   <link rel="stylesheet" href="/seiran/css/book/confirm_publish.css">
 </head>
 
 <body>
-<?php require_once '../component/header.php'; ?>
-<main>
-    <p class="title1">投稿前確認</p>
-    <p>以下の内容で投稿します。よろしいですか？</p>
-<div class="box">
-    <p class="title2">タイトル</p>
-    <p>ここにタイトル</p>
-    <p class="title3">概要</p>
-    <p>これはテストデータの説明です。</p>
-</div>
-    <a href="#" class="button is-primary">投稿</a> &emsp;
-    <a href="#" class="button is-link is-outlined">書き直す</a>
-</main>
+  <?php require_once '../component/header.php'; ?>
+  <main>
+    <div class="container">
+      <p class="title1">投稿前確認</p>
+      <p>以下の内容で投稿します。よろしいですか？</p>
+    </div>
+
+    <div class="box p-5 my-5">
+      <div class="container">
+        <p class="title2">タイトル</p>
+        <p>ここにタイトル</p>
+      </div>
+      <p class="title3">概要</p>
+      <p>これはテストデータの説明です。</p>
+    </div>
+
+    <div class="container">
+      <button onclick="location.href='#'" class="button is-primary">投稿</button>
+      <button onclick="location.href='#'" class="button is-link is-outlined">書き直す</button>
+    </div>
+  </main>
 </body>
 
 </html>
