2e138f51c3684de5f2734ca3b12557bc7050e8e2
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-22 12:17:45 +0900

    update: これでいいや

--- view/book/editor.php
+++ view/book/editor.php
@@ -12,14 +12,24 @@
 <body>
   <div class="columns">
     <div class="sidebar column is-one-fifth">
-      <aside class="menu">
+      <aside class="menu pl-6 pt-2">
+        <div class="menu-list">
+          <i class="fa-solid fa-house"></i>
+          <h1>ホーム</h1>
+        </div>
+        <div class="menu-list">
+          <h2>エディタ</h2>
+        </div>
+        <div class="menu-list">
+          <h2>下書き一覧</h2>
+        </div>
       </aside>
     </div>
     <div class="column">
       <div class="content">
         <form action="#" method="POST">
           <div class="content-top pr-3">
-            <input class="input" type="text" placeholder="タイトルを入力">
+            <input class="input-title input" type="text" placeholder="タイトルを入力">
             <button class="button is-primary">公開設定</button>
           </div>
           <textarea class="input-content textarea" placeholder="本文を入力..."></textarea>
