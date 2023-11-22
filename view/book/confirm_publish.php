673884b50f4748ad006c27bcea5c10d912d6ff9a
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-22 09:54:00 +0900

    delete: 不要なcssの削除

--- view/book/confirm_publish.php
+++ view/book/confirm_publish.php
@@ -8,7 +8,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>公開前確認 | Seiran</title>
   <?php require_once '../component/head.php'; ?>
-  <link rel="stylesheet" href="/seiran/css/book/confirm_publish.css">
 </head>
 
 <body>
