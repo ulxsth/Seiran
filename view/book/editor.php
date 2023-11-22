add1b2e09370c6965b13f2cf0b359623a4967740
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-22 11:23:39 +0900

    update: bulmaで画面を分割した

--- view/book/editor.php
+++ view/book/editor.php
@@ -6,10 +6,22 @@
 <head>
   <?php require_once '../component/head.php'; ?>
   <title>編集中 | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/editor.css">
 </head>
 
 <body>
-  <?php require_once '../component/header.php'; ?>
+  <div class="columns">
+    <div class="column is-one-fifth">
+      <aside class="menu">
+      </aside>
+    </div>
+    <div class="column">
+      <div class="content">
+        <input class="input" type="text" placeholder="タイトルを入力">
+        <textarea class="textarea" placeholder="本文を入力..."></textarea>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
