6e6722dc3bb6592db9a68a38bb1c3c159fdbc43e
sota022 <2201028@s.asojuku.ac.jp>
2023-11-24 10:15:23 +0900

    #23

--- view/user/edit.php
+++ view/user/edit.php
@@ -6,10 +6,25 @@
 <head>
   <?php require_once '../component/head.php'; ?>
   <title>プロフィール編集 | Seiran</title>
+  <link rel="stylesheet" href="/seiran/css/app.css">
+  <link rel="stylesheet" href="/seiran/css/user/edit.css">
 </head>
 
 <body>
   <?php require_once '../component/header.php'; ?>
+<main>
+  <div>
+  <p>user_name</p>
+  <input class="text" type="text" name="" >
+  <button class="button is-primary">変更</button>
+  <p>mail_address</p>
+  <input class="text" type="text" name="" >
+  <button class="button is-primary">変更</button>
+  <p>password</p>
+  <input class="text" type="password" name="" >
+  <button class="button is-primary">変更</button>
+  </div>
+</main>
 </body>
 
 </html>
