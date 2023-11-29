bb009dec2fe3fb52564572e651adfb7b84ca2833
Meiki <mickey.0103.eiking@outlook.jp>
2023-11-29 10:47:29 +0900

    修正

--- view/book/input_detail.php
+++ view/book/input_detail.php
@@ -22,7 +22,7 @@
             <h1>小説を作成する</h1>
         </section>
 
-        <form action="/seiran/src/usecase/usecase/PublishBookUseCase.php" method="POST">
+        <form action="/seiran/src/usecase/book/PublishBookUseCase.php" method="POST">
             <div class="container mb-6">
                 <div class="field">
                     <label for="title">タイトル</label>
