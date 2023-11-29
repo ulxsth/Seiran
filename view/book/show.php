3523f6e0bac4ad70fb02adcdc9f88aecf24e244c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-29 11:05:27 +0900

    fix: パス修正

--- view/book/show.php
+++ view/book/show.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ + '../../src/usecase/book/FindBookByIdUseCase.php';
-require_once __DIR__ + '../../src/usecase/user/FindUserByIdUseCase.php';
+require_once __DIR__ . '/../../src/usecase/book/FindBookByIdUseCase.php';
+require_once __DIR__ . '/../../src/usecase/user/FindUserByIdUseCase.php';
 
 $book = findBookById($_GET['id']);
 
