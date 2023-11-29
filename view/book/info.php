711d0495c9a4ae825ac4cc84b620c8178da9ecf0
sota022 <2201028@s.asojuku.ac.jp>
2023-11-29 13:37:49 +0900

    #11

--- view/book/info.php
+++ view/book/info.php
@@ -2,12 +2,33 @@
 <html lang="ja">
 
 <head>
-  <?php require_once '../component/head.php'; ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ホーム | Seiran</title>
+  <?php require_once '../component/head.php'; ?>
+  <link rel="stylesheet" href="/seiran/css/app.css">
+  <link rel="stylesheet" href="/seiran/css/book/info.css">
 </head>
 
 <body>
-  <?php require_once '../component/header.php'; ?>
+<?php require_once '../component/header.php'; ?>
+  <main>
+    <p class="dai">タイムライン</p>
+    <p class="dai">総合ランキング</p>
+    <div class="flex">
+    <figure class="image"><img src="" alt=""></figure>
+    <div class="right">
+    <p class="title">ここにタイトル</p>
+    <p class="subtitle">これはサブタイトルです</p>
+    <p class="">これは物語の概要です。</p>
+    </div>
+    </div>
+    <p class="dai">カテゴリ別ランキング</p>
+    <p class="">推理</p>
+    <p class="">恋愛</p>
+    <p class="">コメディ</p>
+    <p class="">新着投稿</p>
+  </main>
 </body>
 
 </html>
