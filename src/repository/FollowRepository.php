1769dc5d77bfefa8751b44addd38987b4d3f06b1
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-29 14:56:38 +0900

    fix

--- src/repository/FollowRepository.php
+++ src/repository/FollowRepository.php
@@ -1,32 +1,39 @@
 <?php
-require_once( __DIR__ .'./../util/PdoManager.php');
-require( __DIR__ . '/../dto/FollowDTO.php');
+require_once __DIR__ . '/../util/PdoManager.php';
+require_once __DIR__ . '/../dto/FollowDTO.php';
 
 class FollowRepository {
-  private static $pdo = PdoManager::getPdo();
+  private static $pdo;
+
+  public function __construct() {
+    self::$pdo = PdoManager::getPdo();
+  }
 
   const TABLE_NAME = 'follows';
   const FOLLOWEE_ID = 'followee_id';
   const FOLLOWER_ID = 'follower_id';
 
     /**
-     * フォロー情報を取得する
-     * @param FollowDTO $dto
+     * フォロー情報を新しく追加する
+     * @param string $followeeId
+     * @param string $followerId
      * @return void
      */
-    public function insert($dto) {
-      // SQLの準備
-      $sql = <<<SQL
-      INSERT INTO {self::TABLE_NAME} ({self::FOLLOWEE_ID}, {self::FOLLOWER_ID})
-      VALUES (:user_id, :follow_user_id)
-      SQL;
+  public function insert($followeeId, $followerId) {
+    // SQLの準備
+    $sql = sprintf(
+      "INSERT INTO %s (%s, %s) VALUES (:followee_id, :follower_id)",
+      self::TABLE_NAME,
+      self::FOLLOWEE_ID,
+      self::FOLLOWER_ID
+    );
 
-      // SQLの実行
-      $stmt = self::$pdo->prepare($sql);
-      $stmt->bindValue(':user_id', $dto->getFolloweeId(), PDO::PARAM_INT);
-      $stmt->bindValue(':follow_user_id', $dto->getFollowerId(), PDO::PARAM_INT);
-      $stmt->execute();
-    }
+    // SQLの実行
+    $stmt = self::$pdo->prepare($sql);
+    $stmt->bindValue(':followee_id', $followeeId, PDO::PARAM_STR);
+    $stmt->bindValue(':follower_id', $followerId, PDO::PARAM_STR);
+    $stmt->execute();
+  }
 
     /**
      * フォローしているかどうか取得する
@@ -35,16 +42,17 @@ class FollowRepository {
      */
     public function isExist($dto) {
       // SQLの準備
-      $sql = <<<SQL
-      SELECT * FROM {self::TABLE_NAME}
-      WHERE {self::FOLLOWEE_ID} = :user_id
-      AND {self::FOLLOWER_ID} = :follow_user_id
-      SQL;
+      $sql = sprintf(
+        "SELECT * FROM %s WHERE %s = :user_id AND %s = :follow_user_id",
+        self::TABLE_NAME,
+        self::FOLLOWEE_ID,
+        self::FOLLOWER_ID
+      );
 
       // SQLの実行
       $stmt = self::$pdo->prepare($sql);
-      $stmt->bindValue(':user_id', $dto->getFolloweeId(), PDO::PARAM_INT);
-      $stmt->bindValue(':follow_user_id', $dto->getFollowerId(), PDO::PARAM_INT);
+      $stmt->bindValue(':user_id', $dto->getFolloweeId(), PDO::PARAM_STR);
+      $stmt->bindValue(':follow_user_id', $dto->getFollowerId(), PDO::PARAM_STR);
       $stmt->execute();
       $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -59,16 +67,17 @@ class FollowRepository {
     public function delete($dto)
     {
       // SQLの準備
-      $sql = <<<SQL
-      DELETE FROM {self::TABLE_NAME}
-      WHERE {self::FOLLOWEE_ID} = :user_id
-      AND {self::FOLLOWER_ID} = :follow_user_id
-      SQL;
+      $sql = sprintf(
+        "DELETE FROM %s WHERE %s = :user_id AND %s = :follow_user_id",
+        self::TABLE_NAME,
+        self::FOLLOWEE_ID,
+        self::FOLLOWER_ID
+      );
 
       // SQLの実行
       $stmt = self::$pdo->prepare($sql);
-      $stmt->bindValue(':user_id', $dto->getFolloweeId(), PDO::PARAM_INT);
-      $stmt->bindValue(':follow_user_id', $dto->getFollowerId(), PDO::PARAM_INT);
+      $stmt->bindValue(':user_id', $dto->getFolloweeId(), PDO::PARAM_STR);
+      $stmt->bindValue(':follow_user_id', $dto->getFollowerId(), PDO::PARAM_STR);
       $stmt->execute();
     }
   }
