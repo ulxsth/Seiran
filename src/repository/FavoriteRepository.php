618acd046908406ee7c5d178e4f64b20c089bfd8
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-29 12:38:15 +0900

    update: isExistを実装

--- src/repository/FavoriteRepository.php
+++ src/repository/FavoriteRepository.php
@@ -35,6 +35,32 @@ class FavoriteRepository {
   }
 
   /**
+   * 指定したユーザーが、指定した本をいいね済みかどうかを判定する
+   * @param int $userId
+   * @param int $bookId
+   * @return bool
+   */
+  public function isExist($userId, $bookId) {
+    // SQLの準備
+    $sql = sprintf(
+      "SELECT * FROM %s WHERE %s = :user_id AND %s = :book_id",
+      self::TABLE_NAME,
+      self::USER_ID_COLUMN_NAME,
+      self::BOOK_ID_COLUMN_NAME
+    );
+
+    // SQLの実行
+    $stmt = self::$pdo->prepare($sql);
+    $stmt->bindValue(':user_id', $userId, PDO::PARAM_INT);
+    $stmt->bindValue(':book_id', $bookId, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // いいね情報が存在するかどうかで判定する
+    return !empty($result);
+  }
+
+  /**
    * 指定されたidのいいね数を取得する
    * @param int $bookId
    * @return int
