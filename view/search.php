38bc78fc8296a892296d3a5e37caef6221c32ef8
massan02 <tankful-secure0c@icloud.com>
2023-11-29 10:33:26 +0900

    komitto

--- view/search.php
+++ view/search.php
@@ -10,6 +10,16 @@
 
 <body>
   <?php require_once 'component/header.php'; ?>
+  <main>
+    <div class="">
+      <h1>ユーザー</h1>
+    </div>
+  
+
+		<div class="">
+			<h1>小説</h1>
+		</div>
+  </main>
 </body>
 
 </html>
