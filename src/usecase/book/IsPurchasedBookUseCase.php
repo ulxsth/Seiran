7a74ed0ee56165950d803a73c42e7c3892dc435a
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-29 11:51:01 +0900

    fix: sessionの呼び出しを修正

--- src/usecase/book/IsPurchasedBookUseCase.php
+++ src/usecase/book/IsPurchasedBookUseCase.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__, 2) . '/repository/PurchaseRepository.php';
 class IsPurchasedBookUseCase {
   public static function execute() {
     $repository = new PurchaseRepository();
-    return $repository->isPurchased($_SESSION['user_id'], $_GET['id']);
+    return $repository->isPurchased($_SESSION['user']['id'], $_GET['id']);
   }
 }
 ?>
