e578b5b5201a93e61fa9a9c5b1bd92078d28bac3
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-30 11:44:06 +0900

    fix: update関数が二つある意味がないのでidに統一

--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -3,7 +3,8 @@
 require_once __DIR__ . '/../dto/UserDTO.php';
 require_once __DIR__ . '/../util/PdoManager.php';
 
-class UserRepository {
+class UserRepository
+{
     private $pdo;
 
     const TABLE_NAME = 'users';
@@ -15,7 +16,8 @@ class UserRepository {
     const IS_PUBLIC_COLUMN = 'is_public';
     const DESCRIPTION_COLUMN = 'description';
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->pdo = PdoManager::getPdo();
     }
 
@@ -27,7 +29,8 @@ class UserRepository {
      * @param string $password パスワードの平文
      * @return void
      */
-    public function insert($userId,$name,$email,$password) {
+    public function insert($userId, $name, $email, $password)
+    {
         // SQLの準備
         $sql = sprintf(
             "INSERT INTO %s (%s, %s, %s, %s) VALUES (:id, :name, :email, :password_hash)",
@@ -55,7 +58,8 @@ class UserRepository {
      * @param string $id
      * @return UserDTO|null
      */
-    public function findById($id) {
+    public function findById($id)
+    {
         // SQLの準備
         $sql = sprintf(
             "SELECT * FROM %s WHERE %s = :id",
@@ -82,7 +86,8 @@ class UserRepository {
      * @param string $email
      * @return UserDTO|null
      */
-    public function findByEmail($email) {
+    public function findByEmail($email)
+    {
         // SQLの準備
         $sql = sprintf(
             "SELECT * FROM %s WHERE %s = :email",
@@ -134,63 +139,55 @@ class UserRepository {
      * @param UserDto $dto
      * @return void
      */
-    public function updateById($dto) {
+    public function update($dto)
+    {
         // SQLの準備
         $sql = sprintf(
-            "UPDATE %s SET %s = :name, %s = :email, %s = :password_hash WHERE %s = :id",
-            self::TABLE_NAME,
+            "UPDATE %s SET ",
+            self::TABLE_NAME
+        );
+        $columns = [
             self::NAME_COLUMN,
             self::EMAIL_COLUMN,
             self::PASSWORD_HASH_COLUMN,
-            self::ID_COLUMN
-        );
-
-        // SQLの実行
-        $stmt = $this->pdo->prepare($sql);
-        $name = $dto->getName();
-        $email = $dto->getEmail();
-        $password_hash = $dto->getPasswordHash();
-        $id = $dto->getId();
-        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->bindParam(':password_hash', $password_hash, PDO::PARAM_STR);
-        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
-        $stmt->execute();
-    }
-
-    /**
-     * Dtoに登録されたメールアドレスに一致するユーザー情報を更新する
-     * @param UserDTO $dto
-     * @return void
-     */
-    public function updateByEmail($dto) {
-        // SQLの準備
-        $sql = sprintf(
-            "UPDATE %s SET %s = :name, %s = :password_hash WHERE %s = :email",
-            self::TABLE_NAME,
             self::NAME_COLUMN,
-            self::PASSWORD_HASH_COLUMN,
-            self::EMAIL_COLUMN
-        );
+            self::REGISTERED_AT_COLUMN,
+            self::IS_PUBLIC_COLUMN,
+            self::DESCRIPTION_COLUMN
+        ];
+        foreach ($columns as $column) {
+            $sql .= sprintf("%s = :%s, ", $column, $column);
+        }
+        $sql = rtrim($sql, ', ');
+        $sql .= sprintf(" WHERE %s = :id", self::ID_COLUMN);
 
         // SQLの実行
         $stmt = $this->pdo->prepare($sql);
+        $id = $dto->getId();
         $name = $dto->getName();
         $email = $dto->getEmail();
-        $password_hash = $dto->getPasswordHash();
+        $passwordHash = $dto->getPasswordHash();
+        $registeredAt = $dto->getRegisteredAt();
+        $isPublic = $dto->getIsPublic();
+        $description = $dto->getDescription();
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
         $stmt->bindParam(':name', $name, PDO::PARAM_STR);
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->bindParam(':password_hash', $password_hash, PDO::PARAM_STR);
+        $stmt->bindParam(':password_hash', $passwordHash, PDO::PARAM_STR);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':registered_at', $registeredAt, PDO::PARAM_STR);
+        $stmt->bindParam(':is_public', $isPublic, PDO::PARAM_BOOL);
+        $stmt->bindParam(':description', $description, PDO::PARAM_STR);
         $stmt->execute();
     }
-
 
     /**
      * DBアクセスの結果をDtoに変換する
      * @param array $row
      * @return UserDTO
      */
-    private function rowToDto($row) {
+    private function rowToDto($row)
+    {
         if (empty($row)) {
             throw new Exception('$row is empty.');
         }
