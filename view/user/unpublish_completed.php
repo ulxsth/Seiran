0c0d5f46a9e03fd464ce787cad516dee0ae75d1e
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-30 11:56:08 +0900

    fix

--- view/user/unpublish_completed.php
+++ view/user/unpublish_completed.php
@@ -14,18 +14,7 @@
 <body>
   <?php require_once '../component/header.php'; ?>
   <main class="has-text-centered">
-    <figure class="image is-256x256 mx-auto mb-6">
-      <img src="/seiran/assets/img/sendback.png" alt="sendback_icon">
-    </figure>
-
-    <div class="control mb-6">
-      <p>非公開にしました。</p>
-      <p>非公開になった小説はいつでも「下書き一覧」から再投稿できます。</p>
-    </div>
-
-    <div class="control">
-      <button type="submit" class="button is-link is-outlined px-6">ホームへ</button>
-    </div>
+    
   </main>
 
 </html>
