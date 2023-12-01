f6bdb4893486dcb42d6ea0a06dd6f89136b00309
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-01 10:23:08 +0900

    fix: ユーザー認証を最適化

--- src/usecase/auth/LoginWithEmailUseCase.php
+++ src/usecase/auth/LoginWithEmailUseCase.php
@@ -8,14 +8,8 @@ require_once dirname(__FILE__, 3) . "/repository/UserRepository.php";
 $repository = new UserRepository();
 
 // ユーザーが存在するか検索
-$user = $repository->findById($_POST['email']);
-if ($user == null) {
-  // TODO: エラーメッセージを表示
-  header("Location: /seiran/view/auth/login_email.php");
-}
-
-// パスワードが一致するか検証
-if (!password_verify($_POST['password'], $user->getPasswordHash())) {
+$user = $repository->findById($_POST['id']);
+if ($user == null || !password_verify($_POST['password'], $user->getPasswordHash())) {
   // TODO: エラーメッセージを表示
   header("Location: /seiran/view/auth/login_email.php");
 }
