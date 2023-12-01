ce7f90c0446af8e53894362ec0fd51a49fbbe3d9
massan02 <tankful-secure0c@icloud.com>
2023-12-01 09:52:09 +0900

    検索機能　コミット

--- view/search.php
+++ view/search.php
@@ -15,10 +15,21 @@
       <h1>ユーザー</h1>
     </div>
   
+		<div class="mx-2">
 
-		<div class="">
+			<img src="../assets/img/anonimous.svg" alt="user icon">
+			<p>user_id</p>
+			<p>introduce</p>
+			<div class="container has-text-right">フォロー</div>
+		</div>
+
+
+		<div class="mt-4">
 			<h1>小説</h1>
 		</div>
+
+		<div></div>
+
   </main>
 </body>
 
