bbbad3eca740625c89cdc10ddb6143e34f24f738
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-02 12:52:02 +0900

    fix

--- view/book/info.php
+++ view/book/info.php
@@ -15,23 +15,46 @@ session_start();
 </head>
 
 <body>
-<?php require_once '../component/header.php'; ?>
-  <main>
-    <p class="dai">タイムライン</p>
-    <p class="dai">総合ランキング</p>
-    <div class="flex">
-    <figure class="image"><img src="" alt=""></figure>
-    <div class="right">
-    <p class="title">ここにタイトル</p>
-    <p class="subtitle">これはサブタイトルです</p>
-    <p class="text">これは物語の概要です。</p>
+  <?php require_once '../component/header.php'; ?>
+  <main class="has-text-centered">
+    <h1>タイムライン</h1>
+
+
+    <div class="total-ranking">
+      <h1 class="mb-3">総合ランキング</h1>
+      <div class="total-ranking__item is-flex mb-6">
+        <figure class="thumbnail">
+          <img src="https://via.placeholder.com/300x400">
+        </figure>
+        <div class="description has-text-left">
+          <p class="title">ここにタイトル</p>
+          <p class="text">これは物語の概要です</p>
+        </div>
+      </div>
+      <div class="total-ranking__item is-flex mb-6">
+        <figure class="thumbnail">
+          <img src="https://via.placeholder.com/300x400">
+        </figure>
+        <div class="description has-text-left">
+          <p class="title">ここにタイトル</p>
+          <p class="text">これは物語の概要です</p>
+        </div>
+      </div>
+      <div class="total-ranking__item is-flex mb-6">
+        <figure class="thumbnail">
+          <img src="https://via.placeholder.com/300x400">
+        </figure>
+        <div class="description has-text-left">
+          <p class="title">ここにタイトル</p>
+          <p class="text">これは物語の概要です</p>
+        </div>
+      </div>
     </div>
-    </div>
-    <p class="dai">カテゴリ別ランキング</p>
-    <p class="">推理</p>
-    <p class="">恋愛</p>
-    <p class="">コメディ</p>
-    <p class="dai">新着投稿</p>
+    <h1 class="mb-3">カテゴリ別ランキング</h1>
+    <h2 class="mb-3">推理</h2>
+    <h2 class="mb-3">恋愛</h2>
+    <h2 class="mb-3">コメディ</h2>
+    <h1 class="mb-3">新着投稿</h1>
   </main>
 </body>
 
