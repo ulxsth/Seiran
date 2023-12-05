9fae8d1399560b1bea20f9f902554e577a6b0b9c
massan02 <tankful-secure0c@icloud.com>
2023-12-05 14:59:59 +0900

    70%完成

--- view/search.php
+++ view/search.php
@@ -11,61 +11,69 @@ $userDTOList = SearchUserByNameUseCase::execute($name); */
 <html lang="ja">
 
 <head>
-  <?php require('component/head.php'); ?>
-  <title>検索 | Seiran</title>
+	<?php require('component/head.php'); ?>
+	<!-- <link rel="stylesheet" href="/seiran/css/book/search.css"> -->
+	<title>検索 | Seiran</title>
 </head>
 
 <body>
-  <?php require_once 'component/header.php'; ?>
-  <main>
-    <div class="mb-2">
-      <h1>ユーザー</h1>
-    </div>
-  
+	<?php require_once 'component/header.php'; ?>
+	<main>
+		<div class="mb-2">
+			<h1>ユーザー</h1>
+		</div>
+	
 		<?php
 for ($i = 0; $i < 2; $i++) {
-    echo '<div class="columns">';
-    for ($j = 0; $j < 2; $j++) {
-        echo '<div class="column is-flex is-align-items-center">';
-        echo '    <figure class="image is-64x64 mr-2">';
-        echo '        <img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">';
-        echo '    </figure>';
-        echo '    <div class="mt-4">';
-        echo '        <p>user_id</p>';
-        echo '        <p>introduce</p>';
-        echo '    </div>';
-        echo '    <button class="button is-primary px-6 ml-auto">フォロー</button>';
-        echo '</div>';
-    }
-    echo '</div>';
+		echo '<div class="columns">';
+		for ($j = 0; $j < 2; $j++) {
+				echo '<div class="column is-flex is-align-items-center">';
+				echo '    <figure class="image is-64x64 mr-2">';
+				echo '        <img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">';
+				echo '    </figure>';
+				echo '    <div class="mt-4">';
+				echo '        <p>user_id</p>';
+				echo '        <p>introduce</p>';
+				echo '    </div>';
+				echo '    <button class="button is-primary px-6 ml-auto">フォロー</button>';
+				echo '</div>';
+		}
+		echo '</div>';
 }
 ?>
-
 
 		<div class="mt-4">
 			<h1>小説</h1>
 		</div>
 
-		<div class="columns is-flex-direction-row">
-			<div class="column is-flex is-flex-direction-column">
-					<figure class="image is-128x128">
-							<img src="../assets/img/book/sample.png" alt="book image">
-					</figure>
-					<p>Book_name</p>
-					<div class="mb-4">
-						<figure class="image is-32x32">
-							<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">	
-						</figure>
-						<p>test_user</p>
-					</div>
+		<?php
+			for ($i = 0; $i < 1; $i++) {
+				echo '<div class="columns is-flex is-flex-direction-row is-justify-content-space-around">';
+					for ($j = 0; $j <4; $j++){
+									echo '<article class="column is-one-fifth is-flex is-flex-direction-column">';
+							echo '<figure class="image is-4by5">';
+								echo '<img src="../assets/img/book/sample.png" alt="book image">';
+							echo '</figure>';
+								echo '<button>';
+									echo '<img src="../assets/img/bookmark.svg" class="image is-32x32" alt="bookmark icon">';
+								echo '</button>';
+
+							echo '<p class="is-size-5 is-italic">Book_name</p>';
+							echo '<div class="mb-4 is-flex is-flex-direction-row">';
+								echo '<figure class="image is-32x32">';
+									echo '<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">  ';
+								echo '</figure>';
+								echo '<p class="mt-1">test_user</p>';
+								echo '</div>';
+								echo '</article>';
+					}
 					
-					</div>
-				</div>
+				}
 
+				?>
 
-
-
-  </main>
+	</main>
 </body>
 
 </html>
+
