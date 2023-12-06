8d3026db9c60487cfccad5aa66d9cc3825484f7f
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-06 09:57:26 +0900

    refactor: for文を埋め込み形式で書き換え

--- view/search.php
+++ view/search.php
@@ -12,60 +12,56 @@
 <body>
 	<?php require_once 'component/header.php'; ?>
 	<main>
-		<div class="mb-2">
-			<h1>ユーザー</h1>
+		<div class="searched-user">
+			<div class="mb-2">
+				<h1>ユーザー</h1>
+			</div>
+			<?php for ($i = 0; $i < 2; $i++): ?>
+				<div class="columns">
+					<?php for ($j = 0; $j < 2; $j++): ?>
+						<div class="column is-flex is-align-items-center">
+							<figure class="image is-64x64 mr-2">
+								<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">
+							</figure>
+							<div class="mt-4">
+								<p>user_id</p>
+								<p>introduce</p>
+							</div>
+							<button class="button is-primary px-6 ml-auto">フォロー</button>
+						</div>
+					<?php endfor; ?>
+				</div>
+			<?php endfor; ?>
 		</div>
 
-		<?php
-		for ($i = 0; $i < 2; $i++) {
-			echo '<div class="columns">';
-			for ($j = 0; $j < 2; $j++) {
-				echo '<div class="column is-flex is-align-items-center">';
-				echo '    <figure class="image is-64x64 mr-2">';
-				echo '        <img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">';
-				echo '    </figure>';
-				echo '    <div class="mt-4">';
-				echo '        <p>user_id</p>';
-				echo '        <p>introduce</p>';
-				echo '    </div>';
-				echo '    <button class="button is-primary px-6 ml-auto">フォロー</button>';
-				echo '</div>';
-			}
-			echo '</div>';
-		}
-		?>
-
-		<div class="mt-4">
-			<h1>小説</h1>
+		<div class="searched-book">
+			<div class="mt-4">
+				<h1>小説</h1>
+			</div>
+			<?php for ($i = 0; $i < 2; $i++): ?>
+				<div class="columns is-flex is-flex-direction-row is-justify-content-space-around">
+					<?php for ($j = 0; $j < 4; $j++): ?>
+						<article class="column is-one-fifth is-flex is-flex-direction-column">
+							<div class="image-container" style="position: relative;">
+								<figure class="image is-4by5">
+									<img src="../assets/img/book/sample.png" alt="book image">
+								</figure>
+								<button style="position: absolute; bottom: 0; right: 0; border:none">
+									<img src="../assets/img/bookmark.svg" class="image is-32x32" alt="bookmark icon">
+								</button>
+							</div>
+							<p class="is-size-5 is-italic">Book_name</p>
+							<div class="mb-4 is-flex is-flex-direction-row">
+								<figure class="image is-32x32">
+									<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">
+								</figure>
+								<p class="mt-1">test_user</p>
+							</div>
+						</article>
+					<?php endfor; ?>
+				</div>
+			<?php endfor; ?>
 		</div>
-
-		<?php
-		for ($i = 0; $i < 2; $i++) {
-			echo '<div class="columns is-flex is-flex-direction-row is-justify-content-space-around">';
-			for ($j = 0; $j < 4; $j++) {
-				echo '<article class="column is-one-fifth is-flex is-flex-direction-column">';
-				echo '<div class="image-container" style="position: relative;">';
-				echo '<figure class="image is-4by5">';
-				echo '<img src="../assets/img/book/sample.png" alt="book image">';
-				echo '</figure>';
-				echo '<button style="position: absolute; bottom: 0; right: 0; border:none">';
-				echo '<img src="../assets/img/bookmark.svg" class="image is-32x32" alt="bookmark icon">';
-				echo '</button>';
-				echo '</div>';
-				echo '<p class="is-size-5 is-italic">Book_name</p>';
-				echo '<div class="mb-4 is-flex is-flex-direction-row">';
-				echo '<figure class="image is-32x32">';
-				echo '<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">  ';
-				echo '</figure>';
-				echo '<p class="mt-1">test_user</p>';
-				echo '</div>';
-				echo '</article>';
-			}
-			echo '</div>';
-		}
-		?>
-
-
 	</main>
 </body>
 
