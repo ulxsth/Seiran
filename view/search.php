22d2e541c3d3b8726eba9584f7c4d0bfff178378
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-06 10:23:45 +0900

    update: style指定をcssファイルに隔離

--- view/search.php
+++ view/search.php
@@ -7,6 +7,7 @@
 	<title>検索 | Seiran</title>
 	<?php require_once 'component/head.php'; ?>
 	<link rel="stylesheet" href="/seiran/css/app.css">
+	<link rel="stylesheet" href="/seiran/css/search.css">
 </head>
 
 <body>
@@ -34,21 +35,24 @@
 			<?php endfor; ?>
 		</div>
 
-		<div class="searched-book">
+		<div class="searched-books">
 			<div class="mt-4">
 				<h1>小説</h1>
 			</div>
 			<?php for ($i = 0; $i < 2; $i++) : ?>
 				<div class="columns is-flex is-flex-direction-row is-justify-content-space-around">
 					<?php for ($j = 0; $j < 4; $j++) : ?>
-						<article class="column is-one-fifth is-flex is-flex-direction-column">
-							<div class="image-container" style="position: relative;">
-								<figure class="image is-4by5">
-									<img src="../assets/img/book/sample.png" alt="book image">
-								</figure>
-								<div style="position: absolute; bottom: 0; right: 0; border:none">
-									<i class="fa-solid fa-heart"></i>
-								</div>
+						<article class="searched-book column is-one-fifth is-flex is-flex-direction-column">
+							<div class="searched-book__display">
+								<div class="image-container" style="position: relative;">
+									<figure class="image searched-book__thumbnail is-3by4">
+										<img src="../assets/img/book/sample.png" alt="book image">
+									</figure>
+									<div class="searched-book__favorite-display">
+										<i class="fa-solid fa-heart has-text-danger"></i>
+										<span>100</span>
+									</div>
+							</div>
 							</div>
 							<p class="is-size-5 is-italic">Book_name</p>
 							<div class="mb-4 is-flex is-flex-direction-row">
