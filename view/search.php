6189e5dd95124fc4eb46fcea34f7d4ba4f31c303
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-06 10:00:12 +0900

    fix: path

--- view/search.php
+++ view/search.php
@@ -16,12 +16,12 @@
 			<div class="mb-2">
 				<h1>ユーザー</h1>
 			</div>
-			<?php for ($i = 0; $i < 2; $i++): ?>
+			<?php for ($i = 0; $i < 2; $i++) : ?>
 				<div class="columns">
-					<?php for ($j = 0; $j < 2; $j++): ?>
+					<?php for ($j = 0; $j < 2; $j++) : ?>
 						<div class="column is-flex is-align-items-center">
 							<figure class="image is-64x64 mr-2">
-								<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">
+								<img class="is-rounded" src="../assets/img/user/anonimous.svg" alt="user icon">
 							</figure>
 							<div class="mt-4">
 								<p>user_id</p>
@@ -38,22 +38,22 @@
 			<div class="mt-4">
 				<h1>小説</h1>
 			</div>
-			<?php for ($i = 0; $i < 2; $i++): ?>
+			<?php for ($i = 0; $i < 2; $i++) : ?>
 				<div class="columns is-flex is-flex-direction-row is-justify-content-space-around">
-					<?php for ($j = 0; $j < 4; $j++): ?>
+					<?php for ($j = 0; $j < 4; $j++) : ?>
 						<article class="column is-one-fifth is-flex is-flex-direction-column">
 							<div class="image-container" style="position: relative;">
 								<figure class="image is-4by5">
 									<img src="../assets/img/book/sample.png" alt="book image">
 								</figure>
-								<button style="position: absolute; bottom: 0; right: 0; border:none">
-									<img src="../assets/img/bookmark.svg" class="image is-32x32" alt="bookmark icon">
-								</button>
+								<div style="position: absolute; bottom: 0; right: 0; border:none">
+									<i class="fa-solid fa-heart"></i>
+								</div>
 							</div>
 							<p class="is-size-5 is-italic">Book_name</p>
 							<div class="mb-4 is-flex is-flex-direction-row">
 								<figure class="image is-32x32">
-									<img class="is-rounded" src="../assets/img/anonimous.svg" alt="user icon">
+									<img class="is-rounded" src="../assets/img/user/anonimous.svg" alt="user icon">
 								</figure>
 								<p class="mt-1">test_user</p>
 							</div>
