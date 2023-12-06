5d4ffae3dad086f268f2ef00570b95101744699c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-12-06 11:07:39 +0900

    fix: 小説の検索結果が縦並びになってないので修正

--- view/search.php
+++ view/search.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+require_once '../src/usecase/book/FuzzyFetchBookUseCase.php';
+
+$keyword = $_POST['keyword'];
+$books = FuzzyFetchBookUseCase::execute($keyword);
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -39,31 +47,33 @@
 			<div class="mt-4">
 				<h1>小説</h1>
 			</div>
-			<?php for ($i = 0; $i < 2; $i++) : ?>
-				<div class="columns is-flex is-flex-direction-row is-justify-content-space-around">
-					<?php for ($j = 0; $j < 4; $j++) : ?>
-						<article class="searched-book column is-one-fifth is-flex is-flex-direction-column">
-							<div class="searched-book__display">
-								<div class="image-container" style="position: relative;">
-									<figure class="image searched-book__thumbnail is-3by4">
-										<img src="../assets/img/book/sample.png" alt="book image">
-									</figure>
-									<div class="searched-book__favorite-display">
-										<i class="fa-solid fa-heart has-text-danger"></i>
-										<span>100</span>
-									</div>
+			<?php for ($i = 0; $i < count($books); $i++) : ?>
+				<?php if ($i % 4 === 0) : ?>
+					<div class="columns is-flex is-flex-direction-row">
+					<?php endif; ?>
+
+					<article class="searched-book column is-one-fifth is-flex is-flex-direction-column">
+						<div class="searched-book__display">
+							<figure class="image searched-book__thumbnail is-3by4">
+								<img src="../assets/img/book/sample.png" alt="book image">
+							</figure>
+							<div class="searched-book__favorite-display">
+								<i class="fa-solid fa-heart has-text-danger"></i>
+								<span>100</span>
 							</div>
-							</div>
-							<p class="is-size-5 is-italic">Book_name</p>
-							<div class="mb-4 is-flex is-flex-direction-row">
-								<figure class="image is-32x32">
-									<img class="is-rounded" src="../assets/img/user/anonimous.svg" alt="user icon">
-								</figure>
-								<p class="mt-1">test_user</p>
-							</div>
-						</article>
-					<?php endfor; ?>
-				</div>
+						</div>
+						<p class="is-size-5 is-italic"><?php echo $books[$i]->getName(); ?></p>
+						<div class="mb-4 is-flex is-flex-direction-row">
+							<figure class="image is-32x32">
+								<img class="is-rounded" src="../assets/img/user/anonimous.svg" alt="user icon">
+							</figure>
+							<p class="mt-1"><?php echo $books[$i]->getUserId(); ?></p>
+						</div>
+					</article>
+
+					<?php if (($i + 1) % 4 === 0 || $i === count($books) - 1) : ?>
+					</div>
+				<?php endif; ?>
 			<?php endfor; ?>
 		</div>
 	</main>
