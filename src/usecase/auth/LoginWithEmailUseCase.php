3b7bd0802321cdb6d9eb68dd32fd9a02bc919d8f
massan02 <tankful-secure0c@icloud.com>
2023-12-06 11:01:00 +0900

    エラーメッセージ追加

--- src/usecase/auth/LoginWithEmailUseCase.php
+++ src/usecase/auth/LoginWithEmailUseCase.php
@@ -11,7 +11,9 @@ $repository = new UserRepository();
 $user = $repository->findByEmail($_POST['email'], true);
 if ($user == null || !password_verify($_POST['password'], $user->getPasswordHash())) {
   // TODO: エラーメッセージを表示
+  $_SESSION['error_message'] = ERR_FAILED;
   header("Location: /seiran/view/auth/login_id.php");
+  exit;
 }
 
 // ログイン処理
