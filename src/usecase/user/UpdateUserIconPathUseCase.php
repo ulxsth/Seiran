e8273a1c83e610467a24d2989e6086c0427c7080
massan02 <tankful-secure0c@icloud.com>
2023-12-07 12:17:41 +0900

    バックのバリデーション実装
    アスペクト比の判定方法がheight==widthだからちょっと修正が必要かも

--- src/usecase/user/UpdateUserIconPathUseCase.php
+++ src/usecase/user/UpdateUserIconPathUseCase.php
@@ -4,8 +4,37 @@ require_once dirname(__DIR__, 2) . "/repository/UserRepository.php";
 
 $icon = $_FILES['icon'];
 if (!is_uploaded_file($icon['tmp_name'])) {
+  $_SESSION['error_message'] = 'ファイルがアップロードされていません。';
   header('Location: /seiran/view/user/edit.php?id=' . $user->getId());
   exit;
+}
+
+// アップロードされたファイルが画像であることを確認する
+if (getimagesize($icon['tmp_name']) === false) {
+  $_SESSION['error_message'] = 'ファイルが画像ではありません。';
+  header('Location: /seiran/view/user/edit.php?id=' . $user->getId());
+  exit;
+}
+
+// アップロードされたファイルが許容されるファイル形式であることを確認する
+$allowed_image_types = array(IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_GIF, IMAGETYPE_BMP);
+$detected_image_type = exif_imagetype($_FILES["fileToUpload"]["tmp_name"]);
+if (!in_array($detected_image_type, $allowed_image_types)) {
+    $_SESSION['error_message'] = "許容されるファイル形式はJPG、JPEG、PNG、GIF、BMPのみです。";
+    exit;
+}
+
+// アップロードされたファイルが許容されるサイズであることを確認する
+$max_file_size = 5000000; // 5MB
+$uploaded_image = $_FILES["fileToUpload"]["tmp_name"];
+list($width, $height, $type, $attr) = getimagesize($uploaded_image);
+if ($width > 2000 || $height > 2000 || $width != $height) {
+    $_SESSION['error_message'] = "画像の幅と高さは2000px以下で、アスペクト比は1:1である必要があります。";
+    exit;
+}
+if ($_FILES["fileToUpload"]["size"] > $max_file_size) {
+  $_SESSION['error_message'] = "ファイルサイズが大きすぎます。5MB以下のファイルをアップロードしてください。";
+    exit;
 }
 
 // ユーザ検索
