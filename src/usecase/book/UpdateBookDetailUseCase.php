e7ab2170835e52052c1a32b1cae521589c0a742c
massan02 <tankful-secure0c@icloud.com>
2023-12-12 14:43:59 +0900

    エラーが出なかったので適用

--- src/usecase/book/UpdateBookDetailUseCase.php
+++ src/usecase/book/UpdateBookDetailUseCase.php
@@ -24,6 +24,7 @@ if (!is_uploaded_file($thumbnail['tmp_name'])) {
 $image_info = getimagesize($thumbnail['tmp_name']);
 if ($image_info === false) {
   $_SESSION['error_message'] = 'アイコン:ファイルが画像ではありません。';
+  header('Location: /seiran/view/book/edit_detail.php?id=' . $_GET['id'] . '#thumbnail');
   exit;
 }
 
@@ -32,6 +33,7 @@ $allowed_image_types = array(IMAGETYPE_JPEG, IMAGETYPE_PNG, IMAGETYPE_GIF, IMAGE
 $detected_image_type = $image_info[2];
 if (!in_array($detected_image_type, $allowed_image_types)) {
     $_SESSION['error_message'] = "アイコン:許容されるファイル形式はJPG、JPEG、PNG、GIF、BMPのみです。";
+    header('Location: /seiran/view/book/edit_detail.php?id=' . $_GET['id'] . '#thumbnail');
     exit;
 }
 
@@ -39,10 +41,12 @@ if (!in_array($detected_image_type, $allowed_image_types)) {
 $max_file_size = 5000000; // 5MB
 if ($image_info[0] > 2000 || $image_info[1] > 2000 || ($image_info[0] / 3) != ($image_info[1] / 4)) {
     $_SESSION['error_message'] = "アイコン:画像の幅と高さは2000px以下で、アスペクト比は3:4である必要があります。";
+    header('Location: /seiran/view/book/edit_detail.php?id=' . $_GET['id'] . '#thumbnail');
     exit;
 }
 if ($thumbnail["size"] > $max_file_size) {
   $_SESSION['error_message'] = "アイコン:ファイルサイズが大きすぎます。5MB以下のファイルをアップロードしてください。";
+  header('Location: /seiran/view/book/edit_detail.php?id=' . $_GET['id'] . '#thumbnail');
     exit;
 }
 
