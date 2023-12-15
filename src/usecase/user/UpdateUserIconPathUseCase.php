b673b8d0433269a345d016cc8544ffcd82440a61
massan02 <tankful-secure0c@icloud.com>
2023-12-15 14:52:26 +0900

    修正6

--- src/usecase/user/UpdateUserIconPathUseCase.php
+++ src/usecase/user/UpdateUserIconPathUseCase.php
@@ -3,6 +3,9 @@ session_start();
 require_once dirname(__DIR__, 2) . "/repository/UserRepository.php";
 
 $icon = $_FILES['icon'];
+$repository = new UserRepository();
+$user = $repository->findById($_POST['id']);
+
 if (!is_uploaded_file($icon['tmp_name'])) {
   $_SESSION['error_message'] = 'アイコン:ファイルがアップロードされていません。';
   header('Location: /seiran/view/user/edit.php?id=' . $user->getId());
@@ -31,7 +34,7 @@ $uploaded_image = $_FILES["icon"]["tmp_name"];
 list($width, $height, $type, $attr) = getimagesize($uploaded_image);
 if ($width > 2000 || $height > 2000 || $width != $height) {
     $_SESSION['error_message'] = "アイコン:画像の幅と高さは2000px以下で、アスペクト比は1:1である必要があります。";
-    header('Location: /seiran/view/user/edit.php?id=' . $_GET['id']);
+    header('Location: /seiran/view/user/edit.php?id=' . $user->getId());
     exit;
 }
 if ($_FILES["icon"]["size"] > $max_file_size) {
@@ -41,8 +44,7 @@ if ($_FILES["icon"]["size"] > $max_file_size) {
 }
 
 // ユーザ検索
-$repository = new UserRepository();
-$user = $repository->findById($_POST['id']);
+
 if (is_null($user)) {
   header('Location: /seiran/view/error/404.php');
   exit;
