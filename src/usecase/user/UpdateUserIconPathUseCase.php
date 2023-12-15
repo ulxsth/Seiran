c34d1844ca4c5c51f9e0bbd1cb67f10c32ac3eb8
massan02 <tankful-secure0c@icloud.com>
2023-12-15 14:44:34 +0900

    修正3

--- src/usecase/user/UpdateUserIconPathUseCase.php
+++ src/usecase/user/UpdateUserIconPathUseCase.php
@@ -26,13 +26,13 @@ if (!in_array($detected_image_type, $allowed_image_types)) {
 
 // アップロードされたファイルが許容されるサイズであることを確認する
 $max_file_size = 5000000; // 5MB
-$uploaded_image = $_FILES["fileToUpload"]["tmp_name"];
+$uploaded_image = $_FILES["icon"]["tmp_name"];
 list($width, $height, $type, $attr) = getimagesize($uploaded_image);
 if ($width > 2000 || $height > 2000 || $width != $height) {
     $_SESSION['error_message'] = "アイコン:画像の幅と高さは2000px以下で、アスペクト比は1:1である必要があります。";
     exit;
 }
-if ($_FILES["fileToUpload"]["size"] > $max_file_size) {
+if ($_FILES["icon"]["size"] > $max_file_size) {
   $_SESSION['error_message'] = "アイコン:ファイルサイズが大きすぎます。5MB以下のファイルをアップロードしてください。";
     exit;
 }
